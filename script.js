37d50a91014c5b127a8130c8e151b319159f2746
julia <j.felberbauer@fesios.com>
2023-11-25 20:31:00 +0100

    Added CSS animation practice exercises

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello,world!");
+
+const testButton = document.querySelector(".test-button");
+testButton.addEventListener("click", function() {
+    if (this.style.backgroundColor === "salmon") {
+        Object.assign(this.style, {
+            backgroundColor: "white",
+            color: "salmon",
+            // marginBottom: "20px",
+        });
+    } else {
+        this.style.backgroundColor = "salmon";
+        this.style.color = "white";
+        // this.style.marginBottom = "10px";
+    }
+});
+
+const stripe = document.querySelector("#stripe");
+stripe.addEventListener("click", function() {
+    this.classList.add("animate");
+});
